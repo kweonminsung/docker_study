f48d671ddd2f175d779ee4239f4b65a6d491787e
kweonminsung <kevin6528@scg.skku.ac.kr>
2022-06-10 12:50:32 +0900

    test

--- backend/main.py
+++ backend/main.py
@@ -4,13 +4,9 @@ from datetime import datetime
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:3000"
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
